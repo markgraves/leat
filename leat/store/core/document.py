0f84079528a0bbdd72c15ee79a81155e6430e28a
markgraves <mgraves@xweave.com>
2023-02-25 18:07:47 -0800

    doc dict hash

--- leat/store/core/document.py
+++ leat/store/core/document.py
@@ -1,5 +1,6 @@
 """Classes for Documents."""
 
+import hashlib
 from typing import Optional
 
 
@@ -9,3 +10,28 @@ class Document:
     def __init__(self, name: str, text: Optional[str] = ""):
         self.name = name
         self.text = text
+        self.sha256 = None
+
+    def to_dict(self, include_text=False, use_hash=True):
+        d = {}
+        d["name"] = self.name
+        if include_text:
+            d["text"] = self.text
+        if use_hash:
+            if self.sha256 is None:
+                if self.text:
+                    self.sha256 = hashlib.sha256(self.text.encode())
+                    d["sha256"] = self.sha256.hexdigest()
+            else:
+                d["sha256"] = self.sha256.hexdigest()
+        return d
+
+    @classmethod
+    def from_dict(cls, d):
+        """Create a Document from a dict"""
+        name = d.get("name", "")
+        text = d.get("text", "")
+        sha256 = d.get("sha256")
+        doc = cls(name, text)
+        doc.sha256 = sha256
+        return doc
