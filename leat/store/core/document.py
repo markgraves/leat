43b93311b0e0b9a5610ea217eaffa3066fd03ffc
markgraves <mgraves@xweave.com>
2023-02-28 18:09:16 -0800

    fix document dict roundtrip issue

--- leat/store/core/document.py
+++ leat/store/core/document.py
@@ -17,13 +17,20 @@ class Document:
         d["name"] = self.name
         if include_text:
             d["text"] = self.text
+        if hasattr(self, "text_length") and not self.text:
+            d["text_length"] = self.text_length
+        else:
+            d["text_length"] = len(self.text)
         if use_hash:
             if self.sha256 is None:
                 if self.text:
                     self.sha256 = hashlib.sha256(self.text.encode())
                     d["sha256"] = self.sha256.hexdigest()
             else:
-                d["sha256"] = self.sha256.hexdigest()
+                if isinstance(self.sha256, str):
+                    d["sha256"] = self.sha256
+                else:
+                    d["sha256"] = self.sha256.hexdigest()
         return d
 
     @classmethod
@@ -34,4 +41,14 @@ class Document:
         sha256 = d.get("sha256")
         doc = cls(name, text)
         doc.sha256 = sha256
+        text_length = d.get("text_length")
+        if text_length is not None:
+            if text and text_length != len(text):
+                print(
+                    "WARNING:",
+                    f"Inconsistent text length {text_length} != {len(text)} for",
+                    name,
+                )
+            else:
+                doc.text_length = text_length
         return doc
