8a5f2545b0b1d48a82ec40703f22ce1601b896f3
markgraves <mgraves@xweave.com>
2023-04-07 12:18:38 -0700

    document search

--- leat/search/search.py
+++ leat/search/search.py
@@ -2,7 +2,7 @@
 
 from collections import defaultdict
 from pathlib import Path
-from typing import Optional, Union
+from typing import Optional, Union, List
 import re
 
 from .config import ConfigData
@@ -13,7 +13,33 @@ from leat.store.filesys import LocalFileSys
 
 
 class Search:
-    "Search document stores for files that match configured search patterns"
+    """Search document stores for files that match configured search patterns
+
+    Attributes:
+      config: ConfigData | Path | str | None: Configuration data, or path to it
+      doc_store: DocStore | Path | list | str | None: Document store, path(s) to it, or a file to be searched
+      predefined_configuration: str | None:  If not None, use predefined configuration for config
+      default_section_sep: int | None: Length of text without patterns that is sufficient to create a new section or results
+      default_section_max: int: Maximum length of a section. Useful if downstream data structures have a max length, e.g., deep learning tensors. Ignore if 0.
+      sparse_data: bool: Whether spans are sparse in doc store. Used for efficiency considerations.
+
+    Search takes a DocStore and ConfigData and generates DocResult for each document. Combines functionality of:
+      - DocStore - Documents to be searched
+      - ConfigData - Match patterns to be used in the search
+      - PatternBuilder - Builds the match patterns from config data
+      - Doing the actual search, here
+      - DocResult - Structuring the results for each document
+
+    Examples::
+
+      doc_results = [doc_result in Search(configfile, document_directory)]
+      doc_results = [doc_result for doc_result in Search(predefined_configuration='BasicSearch', doc_store=".")]
+      search = Search(predefined_configuration='BasicSearch',
+                doc_store="leat/tests/data/docset1/simple-document-1.txt")
+      search.all_concepts()
+      list(search.search_documents())
+
+    """
 
     def __init__(
         self,
@@ -22,8 +48,19 @@ class Search:
         predefined_configuration: Optional[str] = None,
         default_section_sep: Optional[int] = 125,
         default_section_max: int = 0,
-        sparse_data=False,
+        sparse_data: bool = False,
     ):
+        """
+        Searches document stores for configured search patterns
+
+        Args:
+          config: ConfigData | Path | str | None: Configuration data, or path to it (Default value = None)
+          doc_store: DocStore | Path | list | str | None: Document store, path(s) to it, or a file to be searched
+          predefined_configuration: str | None:  If not None, use predefined configuration for config (Default value = None)
+          default_section_sep: int | None: Length of text without patterns that is sufficient to create a new section or results (Default value = 125)
+          default_section_max: int: Maximum length of a section. Useful if downstream data structures have a max length, e.g., deep learning tensors. Ignore if 0 (Default value = 0)
+          sparse_data: bool: Whether spans are sparse in doc store. Used for efficiency considerations. (Default value = False)
+        """
         self.match_patterns: Optional[list] = None
         self.sparse_data = sparse_data
         self._super_pattern = None
@@ -36,11 +73,18 @@ class Search:
         self.default_section_max = default_section_max
 
     @property
-    def config(self):
+    def config(self) -> Optional[ConfigData]:
+        """Config data"""
         return self._config
 
     @config.setter
     def config(self, config: Optional[Union[ConfigData, Path, str]]):
+        """
+        Sets config
+
+        Args:
+          config: ConfigData | Path | str | None: Configuration data, or path to it (Default value = None)
+        """
         if config is None:
             self._config = None
             return
@@ -61,11 +105,18 @@ class Search:
             self._super_pattern = re.compile(super_pattern, flags=flag)
 
     @property
-    def doc_store(self):
+    def doc_store(self) -> Optional[DocStore]:
+        """Doc store"""
         return self._doc_store
 
     @doc_store.setter
     def doc_store(self, doc_store):
+        """
+        Sets doc store
+
+        Args:
+          doc_store: DocStore | Path | list | str | None: Document store, path(s) to it, or a file to be searched
+        """
         if doc_store is None:
             self._doc_store = DocStore()
             return
@@ -78,7 +129,7 @@ class Search:
             if filepath.is_dir():
                 print("INFO:", "Creating doc store in search for directory:", doc_store)
                 self._doc_store = DocStore(LocalFileSys(filepath))
-            elif filepath.is_file():
+            elif filepath.expanduser().is_file():
                 lfs = LocalFileSys()
                 lfs.add_directory(
                     filepath.parent, include=filepath.name, recursive=False
@@ -95,7 +146,16 @@ class Search:
     def search_documents(
         self, section_sep: Optional[int] = None, section_max: Optional[int] = None
     ):
-        "Search all documents from document store for all match patterns"
+        """
+        Search all documents from document store for all match patterns
+
+        Args:
+          section_sep: int | None: Length of text without patterns that is sufficient to create a new section or results  (Default value = None)
+          section_max: int | None: Maximum length of a section. Useful if downstream data structures have a max length, e.g., deep learning tensors. Ignore if 0 or None. (Default value = None)
+
+        Yields:
+          DocResult: Result for each document that has one or more matches
+        """
         if section_sep is None:
             section_sep = self.default_section_sep
         if section_max is None:
@@ -108,22 +168,43 @@ class Search:
             yield from []
         else:
             for doc in self.doc_store:
-                yield self.search_document(
+                result = self.search_document(
                     doc, section_sep=section_sep, section_max=section_max
                 )
+                if result is not None:
+                    yield result
 
     def search_document(
         self,
         doc: Document,
         section_sep: Optional[int] = None,
         section_max: Optional[int] = None,
-    ):
-        "Search a document for all match patterns"
+    ) -> Optional[DocResult]:
+        """
+        Search a document for all match patterns
+
+        Args:
+          doc: Document: Document to search
+          section_sep: int | None: Length of text without patterns that is sufficient to create a new section or results  (Default value = None)
+          section_max: int | None: Maximum length of a section. Useful if downstream data structures have a max length, e.g., deep learning tensors. Ignore if 0 or None. (Default value = None)
+
+        Returns:
+          DocResult | None: Result for each document, or None if no matches
+        """
         # print("INFO:", doc.name, len(doc.text))
+        if not doc:
+            return
+        if not doc.text:
+            if doc.sha256:
+                print("WARNING:", "Text no longer available for doc", doc.name)
+            else:
+                print("INFO:", "Skipping empty doc", doc.name)
+            return
         if (
             self._super_pattern is not None
             and self._super_pattern.search(doc.text) is None
         ):
+            # If no matches from super_pattern, then no need to iterate over individual matches
             return
         if section_sep is None:
             section_sep = self.default_section_sep
@@ -141,8 +222,17 @@ class Search:
 
     def read_search_document(
         self, file: Union[Path, str], section_sep: Optional[int] = None
-    ):
-        "Read a file and search the document for all match patterns"
+    ) -> Optional[DocResult]:
+        """
+        Read a file and search the document for all match patterns
+
+        Args:
+          file: Path | str: File to search
+          section_sep: Optional[int]: Length of text without patterns that is sufficient to create a new section or results (Default value = None)
+
+        Returns:
+          DocResult | None: Result for each document, or None if no matches
+        """
         doc = self.doc_store.read_document(Path(file))
         if doc is None:
             print("WARN:", "Skipping", file)
@@ -151,15 +241,27 @@ class Search:
 
     def search_document_text(
         self, text: str, name: str = "No Name", section_sep: Optional[int] = None
-    ):
-        "Search the text of a document for all match patterns"
+    ) -> Optional[DocResult]:
+        """
+        Search an arbitrary text for all match patterns. Useful for searching texts not in doc store.
+
+        Args:
+          text: str: Text to search
+          name: str: Name to give to created Document (Default value = "No Name")
+          section_sep: int | None:  Length of text without patterns that is sufficient to create a new section or result. Ignore if 0 or None. (Default value = None)
+
+        Returns:
+          DocResult | None: Result for the text, or None if no matches
+        """
         doc = Document(name, text)
         return self.search_document(doc, section_sep)
 
     def __iter__(self):
+        """Iterate over a search of all documents, from the doc store"""
         return self.search_documents()
 
     def __str__(self):
+        """String representation of the object instance"""
         try:
             datadirs = [str(p.path) for p in self.doc_store.filesys.datadirs]
         except AttributeError:
@@ -169,6 +271,6 @@ class Search:
         except AttributeError:
             return f'<{__class__.__name__} None(0) {",".join(datadirs)[:30]}>'
 
-    def all_concepts(self):
+    def all_concepts(self) -> List[str]:
         """Returns list of all concepts to be searched"""
         return list(set(mp.concept for mp in self.match_patterns))
